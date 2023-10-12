c6e8b56c0d16a7e3a2e9e052b60a76128b7248e5
AgustinaFernandez01 <agustinafer29@gmail.com>
2023-10-12 09:18:28 -0300

    Changes

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -1,22 +1,23 @@
 import { Routes, Route } from 'react-router-dom';
-import Navbar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 import About from "./views/About";
 import Projects from "./views/Projects";
 import Skills from "./views/Skills";
 import Contact from "./views/Contact";
+import { BrowserRouter } from 'react-router-dom';
 
 
 export default function App() {
   return (
-    <>
-      <Navbar />
+    <BrowserRouter>
+      <NavBar/>
       <Routes>
-        <Route path="/" element={<About />} />
+        <Route path="/" element={<About/>} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </>
+    </BrowserRouter>
   );
 }
 
